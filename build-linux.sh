5c5e9631b586639478d7d9b72d647cd6a1ec0d52
Sam Macbeth <sam@cliqz.com>
2020-07-27 17:16:02 +0200

    Use subshell in build scripts

--- build-linux.sh
+++ build-linux.sh
@@ -5,11 +5,11 @@ ROOT=`pwd`
 # copy resources from mozilla source which we need for docker build
 cp ./mozilla-release/taskcluster/scripts/misc/fetch-content ./build/
 
-cd build
-docker build -f Base.dockerfile -t ua-build-base:debian9 ./ --build-arg user=`whoami` --build-arg UID=`id -u` --build-arg GID=`id -g` --build-arg DOCKER_BASE_IMAGE=debian:9
-docker build -f Linux.dockerfile -t ua-build-linux ./
+( cd build &&
+  docker build -f Base.dockerfile -t ua-build-base:debian9 ./ --build-arg user=`whoami` --build-arg UID=`id -u` --build-arg GID=`id -g` --build-arg DOCKER_BASE_IMAGE=debian:9 &&
+  docker build -f Linux.dockerfile -t ua-build-linux ./
+)
 
-cd ../
 docker run -v $ROOT/mozilla-release:/builds/worker/workspace \
   --env MOZCONFIG=/builds/worker/configs/linux.mozconfig \
   -it ua-build-linux \
