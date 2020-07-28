e77704c35ffec203716bc4ba71988e1a61778261
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-07-28 14:14:48 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-echo 'Downloading Firefox release source'
-FIREFOX_RELEASE=78.0
-wget https://archive.mozilla.org/pub/firefox/releases/$FIREFOX_RELEASE/source/firefox-$FIREFOX_RELEASE.source.tar.xz -O tmp/firefox.tar.xz
-tar -xf tmp/firefox.tar.xz -C ./
-mv firefox-$FIREFOX_RELEASE mozilla-release
