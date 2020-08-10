ceded1c746465f8e66b6e0332475d9ec78f46421
Rémi Berson <remi@cliqz.com>
2020-08-10 14:41:19 +0200

    Implement export-patches and import-patches commands

--- fern/core/git.js
+++ fern/core/git.js
@@ -1,7 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+
 const execa = require("execa");
 const Listr = require("listr");
+const rimraf = require("rimraf");
 
-const { withCwd } = require("./utils.js");
+const { withCwd, folderExists, fileExists } = require("./utils.js");
 
 async function setup(version, folder) {
   return new Listr([
@@ -49,7 +53,76 @@ async function reset(version, folder) {
     },
     {
       title: "checkout -b workspace",
-      task: () => withCwd(folder, () => execa("git", ["checkout", "-b", "workspace"])),
+      task: () =>
+        withCwd(folder, () => execa("git", ["checkout", "-b", "workspace"])),
+    },
+  ]);
+}
+
+function exportPatches(root, version) {
+  const patchesFolder = path.join(root, "patches");
+
+  return new Listr([
+    {
+      title: "Reset 'patches' folder",
+      task: async () => {
+        rimraf.sync("patches");
+        await fs.promises.mkdir("patches");
+      },
+    },
+    {
+      title: "Export patches",
+      task: () =>
+        withCwd("mozilla-release", async () => {
+          await execa("git", [
+            "format-patch",
+            `${version}`,
+            "--output-directory",
+            patchesFolder,
+          ]);
+
+          await fs.promises.writeFile(
+            path.join(patchesFolder, ".index"),
+            (await fs.promises.readdir(patchesFolder))
+              .filter((filename) => filename.endsWith(".patch"))
+              .sort()
+              .join("\n"),
+            "utf-8"
+          );
+        }),
+    },
+  ]);
+}
+
+function importPatches(root) {
+  const patchesFolder = path.join(root, "patches");
+  const patchesIndex = path.join(patchesFolder, ".index");
+
+  return new Listr([
+    {
+      title: "Import patches",
+      task: () =>
+        withCwd("mozilla-release", async () => {
+          if ((await folderExists(patchesFolder)) === false) {
+            throw new Error("Patches folder does not exist.");
+          }
+
+          if ((await fileExists(patchesIndex)) === false) {
+            throw new Error("Patches index file does not exist.");
+          }
+
+          const patches = (await fs.promises.readFile(patchesIndex, "utf-8"))
+            .trim()
+            .split("\n")
+            .map((filename) => path.join(patchesFolder, filename));
+
+          await execa("git", [
+            "am",
+            "--ignore-space-change",
+            "--ignore-whitespace",
+            ...patches,
+          ]);
+        }),
     },
   ]);
 }
@@ -57,4 +130,6 @@ async function reset(version, folder) {
 module.exports = {
   setup,
   reset,
+  exportPatches,
+  importPatches,
 };
