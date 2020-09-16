68e5207b237796bdc6dabbc702b760ca0c0c6e13
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-09-16 19:28:17 +0200

    Add certificates for MAR verification - fix #106 (#216)

--- fern/core/managed-patches.js
+++ fern/core/managed-patches.js
@@ -4,10 +4,11 @@ const Listr = require("listr");
 const version = require("../patches/app-version.js");
 const branding = require('../patches/ghostery-branding.js');
 const ghostery = require('../patches/ghostery-extension.js');
+const certificates = require('../patches/certificates.js');
 
 const { withCwd } = require("./utils.js");
 
-const PATCHES = [version, ghostery, branding];
+const PATCHES = [version, ghostery, branding, certificates];
 
 async function commitChanges(patch) {
   await withCwd("mozilla-release", async () => {
---
+++ fern/patches/certificates.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+const fsExtra = require("fs-extra");
+
+const { getRoot } = require("../core/workspace.js");
+
+async function getPathToSourceCertificates() {
+  const root = await getRoot();
+  return path.join(root, "brands", "ghostery", "certificates");
+}
+
+const updaterPathComponents = ["toolkit", "mozapps", "update", "updater"];
+
+async function getPathToTargetCertificates() {
+  const root = await getRoot();
+  return path.join(root, "mozilla-release", ...updaterPathComponents);
+}
+
+module.exports = {
+  name: "Setup certificates",
+  paths: [path.join(...updaterPathComponents)],
+  skip: async () => false,
+  apply: async () => {
+    await fsExtra.copy(
+      await getPathToSourceCertificates(),
+      await getPathToTargetCertificates()
+    );
+  },
+};
