59c9f221ca279c442c3278c5244bf9e8d5968553
Sam Macbeth <sammacbeth@users.noreply.github.com>
2020-09-21 10:02:03 +0200

    Fix missing semicolon

--- brands/ghostery/branding/pref/firefox-branding.js
+++ brands/ghostery/branding/pref/firefox-branding.js
@@ -122,4 +122,4 @@ pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false
 pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
 
 // Remove addons.mozilla.org from set of domains that extensions cannot access
-pref("extensions.webextensions.restrictedDomains", "accounts-static.cdn.mozilla.net,accounts.firefox.com,addons.cdn.mozilla.net,api.accounts.firefox.com,content.cdn.mozilla.net,discovery.addons.mozilla.org,install.mozilla.org,oauth.accounts.firefox.com,profile.accounts.firefox.com,support.mozilla.org,sync.services.mozilla.com")
+pref("extensions.webextensions.restrictedDomains", "accounts-static.cdn.mozilla.net,accounts.firefox.com,addons.cdn.mozilla.net,api.accounts.firefox.com,content.cdn.mozilla.net,discovery.addons.mozilla.org,install.mozilla.org,oauth.accounts.firefox.com,profile.accounts.firefox.com,support.mozilla.org,sync.services.mozilla.com");
