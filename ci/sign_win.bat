31798191be0e530703dbb630803a9ecbfd61d8e0
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-10-06 13:36:32 +0200

    Update sign_win.bat

--- ci/sign_win.bat
+++ ci/sign_win.bat
@@ -40,8 +40,8 @@ for /R %%f in (
   echo Check and sign %%f
   "%CLZ_SIGNTOOL_PATH%" verify /pa %%f
   if ERRORLEVEL 1 (
-    "%CLZ_SIGNTOOL_PATH%" sign /t %timestamp_server_sha1% /f %WIN_CERT% /p "%WIN_CERT_PASS%" %%f
-    "%CLZ_SIGNTOOL_PATH%" sign /fd sha256 /tr %timestamp_server_sha256% /td sha256 /f %WIN_CERT% /p "%WIN_CERT_PASS%" /as %%f
+    "%CLZ_SIGNTOOL_PATH%" sign /t %timestamp_server_sha1% /f %WIN_CERT% /p %%WIN_CERT_PASS%% %%f
+    "%CLZ_SIGNTOOL_PATH%" sign /fd sha256 /tr %timestamp_server_sha256% /td sha256 /f %WIN_CERT% /p %%WIN_CERT_PASS%% /as %%f
     "%CLZ_SIGNTOOL_PATH%" verify /pa %%f
   )
   if ERRORLEVEL 1 (goto :error)
