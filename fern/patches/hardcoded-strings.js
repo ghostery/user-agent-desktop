752fc8677476e295a56d46ccfeb3dcc543a045dd
Sam Macbeth <sammacbeth@users.noreply.github.com>
2020-10-15 19:37:19 +0200

    Replace Firefox strings in appstrings.properties (#287)

---
+++ fern/patches/hardcoded-strings.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const fsExtra = require("fs-extra");
+const { getRoot } = require("../core/workspace.js");
+
+const files = [
+  ["browser", "locales", "en-US", "chrome", "overrides", "appstrings.properties"]
+]
+
+module.exports = () => ({
+  name: "Rewrite Firefox strings",
+  paths: files.map(p => path.join(...p)),
+  skip: async () => false,
+  apply: async () => {
+    return Promise.all(files.map(async (p) => {
+      const filePath = path.join(await getRoot(), 'mozilla-release', ...p);
+      const contents = await fsExtra.readFile(filePath, 'utf-8');
+      return fsExtra.writeFile(filePath, contents.replace(/Firefox/g, 'Ghostery'));
+    }));
+  },
+});
