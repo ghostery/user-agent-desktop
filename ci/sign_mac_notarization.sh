04403aaac8fd80415ec093326419f4b785bad573
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-11-06 14:50:25 +0100

    Create sign_mac_notarization.sh

---
+++ ci/sign_mac_notarization.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+PKG_DIR="mozilla-release/obj-x86_64-apple-darwin/dist"
+
+ASC_USERNAME="$1"
+ASC_PASSWORD="$2"
+
+BUNDLE_ID="com.cliqz.desktopbrowser"
+BUNDLE_PKG="$PKG_NAME.zip"
+
+# create temporary files
+NOTARIZE_APP_LOG=$(mktemp -t notarize-app)
+NOTARIZE_INFO_LOG=$(mktemp -t notarize-info)
+
+# delete temporary files on exit
+function finish {
+	rm "$NOTARIZE_APP_LOG" "$NOTARIZE_INFO_LOG"
+}
+trap finish EXIT
+
+zip -r "$BUNDLE_PKG" "$PKG_DIR/$PKG_NAME.app"
+
+# submit app for notarization
+if xcrun altool --notarize-app --primary-bundle-id "$BUNDLE_ID" --username "$ASC_USERNAME" --password "$ASC_PASSWORD" --asc-provider EvidonInc -f "$BUNDLE_PKG" > "$NOTARIZE_APP_LOG" 2>&1; then
+	cat "$NOTARIZE_APP_LOG"
+	RequestUUID=$(awk -F ' = ' '/RequestUUID/ {print $2}' "$NOTARIZE_APP_LOG")
+
+	# check status periodically
+	while sleep 60 && date; do
+		# check notarization status
+		if xcrun altool --notarization-info "$RequestUUID" --username "$ASC_USERNAME" --password "$ASC_PASSWORD" > "$NOTARIZE_INFO_LOG" 2>&1; then
+			cat "$NOTARIZE_INFO_LOG"
+
+			# once notarization is complete, run stapler and exit
+			if ! grep -q "Status: in progress" "$NOTARIZE_INFO_LOG"; then
+				xcrun stapler staple "$PKG_DIR/$PKG_NAME.app"
+				exit $?
+			fi
+		else
+			cat "$NOTARIZE_INFO_LOG" 1>&2
+			exit 1
+		fi
+	done
+else
+	cat "$NOTARIZE_APP_LOG" 1>&2
+	exit 1
+fi
