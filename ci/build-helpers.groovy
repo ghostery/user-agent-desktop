31448accde68bd831bfac71d55685e06ab39fac2
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-11-06 14:30:10 +0100

    Update build-helpers.groovy

--- ci/build-helpers.groovy
+++ ci/build-helpers.groovy
@@ -252,7 +252,7 @@ def windows_pre_pkg_signing(name, objDir, artifactGlob) {
                     }
                     
                     bat 'del /s /q  signed.tar'
-                    powershell "Compress-Archive mozilla-release\\${objDir}\\dist\\Ghostery\\* -DestinationPath signed.zip -CompressionLevel Fastest"
+                    powershell "Compress-Archive mozilla-release\\${objDir}\\dist\\bin\\* -DestinationPath signed.zip -CompressionLevel Fastest"
                     stash name: "${name}-signed", includes: 'signed.zip'
                 }
             }
