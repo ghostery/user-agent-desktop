a9bb2189a4db473a02e33e8c869e0b36623d496b
Sam Macbeth <sam@cliqz.com>
2020-11-06 09:20:19 +0100

    Cleanup workspace

--- ci/profileserver_win.sh
+++ ci/profileserver_win.sh
@@ -8,8 +8,10 @@ wget -nv -O clang.tar.gz 'http://kria.cliqz:8080/ipfs/QmchjJhoHRVuiWsM75TspdtTyi
 tar -xf clang.tar.gz
 rm clang.tar.gz
 
+# Clean workspace
+rm -rf firefox Ghostery
+
 # get Firefox source
-rm -rf firefox
 wget -nv -O firefox.tar.xz http://kria.cliqz:8080/ipns/k2k4r8jy3h2kku2sungmzbrp2zpj3yf2js0w98bwbbyiqt2qzomkvyli/firefox/releases/82.0.2/source/firefox-82.0.2.source.tar.xz
 tar -xf firefox.tar.xz
 mv firefox-82.0.2 firefox
@@ -29,3 +31,6 @@ unzip $WORKSPACE/mozilla-release/obj-x86_64-pc-mingw32/dist/*.win64.zip -d ./
 
 mkdir -p $WORKSPACE/Windows64/
 tar -Jcvf $WORKSPACE/Windows64/profdata.tar.xz merged.profdata en-US.log
+
+# Cleanup
+rm -rf $WORKSPACE/mozilla-release $WORKSPACE/firefox $WORKSPACE/clang
