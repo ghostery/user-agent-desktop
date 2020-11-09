89ea1346dec8b8e34951e21c083991fc214ab32b
Sam Macbeth <sam@cliqz.com>
2020-11-09 17:09:26 +0100

    Keep IPFS address of browser resources in workspace

--- fern/core/firefox.js
+++ fern/core/firefox.js
@@ -13,15 +13,14 @@ const { getRoot } = require("./workspace.js");
 const { getCacheDir } = require("./caching.js");
 const { fileExists, folderExists, symlinkExists } = require("./utils.js");
 
-async function use(version, ipfsGateway) {
+async function use(version, ipfsGateway, ipfsAddr) {
   const root = await getRoot();
   const cache = await getCacheDir("firefox", `${version}`);
   const folder = path.join(cache, `firefox-${version}`);
   const archive = path.join(cache, `firefox-${version}.source.tar.xz`);
   const git = path.join(folder, ".git");
-  const baseUrl = ipfsGateway ? `${ipfsGateway}/ipns/k2k4r8jy3h2kku2sungmzbrp2zpj3yf2js0w98bwbbyiqt2qzomkvyli` : "https://archive.mozilla.org/pub";
+  const baseUrl = ipfsGateway ? `${ipfsGateway}${ipfsAddr}` : "https://archive.mozilla.org/pub";
   const url = `${baseUrl}/firefox/releases/${version}/source/firefox-${version}.source.tar.xz`;
-
   return new Listr([
     {
       title: "Download",
