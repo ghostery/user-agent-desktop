ecc1f31c7c3bad726a42511af5b9fd3f75b1b9d1
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-11-09 15:12:45 +0100

    Update build-helpers.groovy

--- ci/build-helpers.groovy
+++ ci/build-helpers.groovy
@@ -345,7 +345,7 @@ def mac_pre_pkg_signing(name, objDir, artifactGlob) {
                         sh "./ci/sign_mac_app.sh"
                         
                         sh 'rm -rf signed.zip'
-                        sh "zip --symlinks -r signed.zip mozilla-release/${objDir}/dist/Ghostery\\ Browser.app"
+                        sh "zip -r signed.zip mozilla-release/${objDir}/dist/Ghostery\\ Browser.app"
                         stash name: "${name}-signed", includes: 'signed.zip'
                     }
                 } finally {
