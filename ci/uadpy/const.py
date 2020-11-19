0486a5306bf17b2ea5681b2e4520fb98f703d647
Sam Macbeth <sam@cliqz.com>
2020-11-19 09:56:30 +0100

    Use get.ghosterybrowser.com for all update urls.

--- ci/uadpy/const.py
+++ ci/uadpy/const.py
@@ -2,6 +2,6 @@ API_ROOT = 'http://balrogadmin.ghosterydev.com/api'
 PRODUCT_NAME = 'Ghostery'
 AUTH0_AUDIENCE = 'ghostery-balrog'
 AUTH0_DOMAIN = 'ghostery-balrog.eu.auth0.com'
-REPOSITORY_BASE_URL = 'https://github.com/ghostery/user-agent-desktop/releases/download'
+REPOSITORY_BASE_URL = 'https://get.ghosterybrowser.com/repo/desktop'
 RELEASE_CHANNELS = ["release"]
 NIGHTLY_RULE_ID=4
