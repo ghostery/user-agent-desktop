36760330572a248eb7ccad8ac0c6bd1f4d0f8d0e
Sam Macbeth <sam@cliqz.com>
2020-11-23 12:03:10 +0100

    Use env variables to trigger stub installer signing

--- ci/sign_win.bat
+++ ci/sign_win.bat
@@ -34,37 +34,43 @@ if exist ./pkg%STUB_PREFIX%_%lang% rmdir /q /s "pkg%STUB_PREFIX%_%lang%"
 dir /b %TOOLTOOL_DIR%
 dir /b c:\mozilla-build
 
-for %%p IN ("", "-stub") do (
-  set STUB_PREFIX=%%p
-  %archivator_exe% l dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer%STUB_PREFIX%.exe
-  %archivator_exe% x -opkg%STUB_PREFIX%_%lang% -y dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer%STUB_PREFIX%.exe
-  if not exist ./pkg%STUB_PREFIX%_%lang% (goto :error)
-  cd pkg%STUB_PREFIX%_%lang%
-  for /R %%f in (
-    *.exe *.dll
-  ) do (
-    rem Check does file already have a digital sign. If not - try to create one
-    echo Check and sign %%f
+%archivator_exe% l dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer%STUB_PREFIX%.exe
+%archivator_exe% x -opkg%STUB_PREFIX%_%lang% -y dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer%STUB_PREFIX%.exe
+if not exist ./pkg%STUB_PREFIX%_%lang% (goto :error)
+cd pkg%STUB_PREFIX%_%lang%
+for /R %%f in (
+  *.exe *.dll
+) do (
+  rem Check does file already have a digital sign. If not - try to create one
+  echo Check and sign %%f
+  "%CLZ_SIGNTOOL_PATH%" verify /pa %%f
+  if ERRORLEVEL 1 (
+    "%CLZ_SIGNTOOL_PATH%" sign /t %timestamp_server_sha1% /f %WIN_CERT% /p %WIN_CERT_PASS% %%f
+    "%CLZ_SIGNTOOL_PATH%" sign /fd sha256 /tr %timestamp_server_sha256% /td sha256 /f %WIN_CERT% /p %WIN_CERT_PASS% /as %%f
     "%CLZ_SIGNTOOL_PATH%" verify /pa %%f
-    if ERRORLEVEL 1 (
-      "%CLZ_SIGNTOOL_PATH%" sign /t %timestamp_server_sha1% /f %WIN_CERT% /p %WIN_CERT_PASS% %%f
-      "%CLZ_SIGNTOOL_PATH%" sign /fd sha256 /tr %timestamp_server_sha256% /td sha256 /f %WIN_CERT% /p %WIN_CERT_PASS% /as %%f
-      "%CLZ_SIGNTOOL_PATH%" verify /pa %%f
-    )
-    if ERRORLEVEL 1 (goto :error)
   )
-
-  rem Prepare usual installer
-  del installer.7z
-  %archivator_exe% a -r -t7z installer.7z -mx -m0=BCJ2 -m1=LZMA:d25 -m2=LZMA:d19 -m3=LZMA:d1 -mb0:1 -mb0s1:2 -mb0s2:3
-  cd ..
-  copy /b ..\other-licenses\7zstub\firefox\7zSD.Win32.sfx + mozilla-release\browser\installer\windows\app.tag + pkg_%lang%\installer.7z dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer.exe
-
-  "%CLZ_SIGNTOOL_PATH%" sign /t %timestamp_server_sha1% /f %WIN_CERT% /p %WIN_CERT_PASS% dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer%STUB_PREFIX%.exe
-  "%CLZ_SIGNTOOL_PATH%" sign /fd sha256 /tr %timestamp_server_sha256% /td sha256 /f %WIN_CERT% /p %WIN_CERT_PASS% /as dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer%STUB_PREFIX%.exe
-  "%CLZ_SIGNTOOL_PATH%" verify /pa dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer%STUB_PREFIX%.exe
   if ERRORLEVEL 1 (goto :error)
 )
+
+if defined STUB_PREFIX goto prepare_stub_installer
+rem Prepare usual installer
+del installer.7z
+%archivator_exe% a -r -t7z installer.7z -mx -m0=BCJ2 -m1=LZMA:d25 -m2=LZMA:d19 -m3=LZMA:d1 -mb0:1 -mb0s1:2 -mb0s2:3
+cd ..
+copy /b ..\other-licenses\7zstub\firefox\7zSD.Win32.sfx + mozilla-release\browser\installer\windows\app.tag + pkg_%lang%\installer.7z dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer.exe
+goto sign_installer
+
+:prepare_stub_installer
+del stub.7z
+%archivator_exe% a -t7z stub.7z setup-stub.exe -mx -m0=BCJ2 -m1=LZMA:d21 -m2=LZMA:d17 -m3=LZMA:d17 -mb0:1 -mb0s1:2 -mb0s2:3
+cd ..
+copy /b ..\mozilla-release\other-licenses\7zstub\firefox\7zSD.Win32.sfx + browser\installer\windows\instgen\stub.tag + pkg%STUB_PREFIX%_%lang%\stub.7z dist\install\sea\cliqz-%ff_exe%.%platform_prefix%.installer-stub.exe
+
+:sign_installer
+"%CLZ_SIGNTOOL_PATH%" sign /t %timestamp_server_sha1% /f %WIN_CERT% /p %WIN_CERT_PASS% dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer%STUB_PREFIX%.exe
+"%CLZ_SIGNTOOL_PATH%" sign /fd sha256 /tr %timestamp_server_sha256% /td sha256 /f %WIN_CERT% /p %WIN_CERT_PASS% /as dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer%STUB_PREFIX%.exe
+"%CLZ_SIGNTOOL_PATH%" verify /pa dist\install\sea\%APP_NAME%-%ff_exe%.%platform_prefix%.installer%STUB_PREFIX%.exe
+if ERRORLEVEL 1 (goto :error)
 
 goto :eof
 
