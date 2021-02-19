f3716507cd96bffd4bf44aa196bc7f617d473799
Krzysztof Jan Modras <chrmod@chrmod.net>
2021-02-19 16:02:48 +0100

    Prefs: bring back mandatory defaults
    The lack of `devtools.selfxss.count` causes devtools to throw errors instead of logging to console.

--- brands/ghostery/branding/pref/firefox-branding.js
+++ brands/ghostery/branding/pref/firefox-branding.js
@@ -182,3 +182,7 @@ pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", f
 pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false);
 pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
 pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false);
+
+// Number of usages of the web console.
+// If this is less than 5, then pasting code into the web console is disabled
+pref("devtools.selfxss.count", 5);
