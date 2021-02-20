b3962ce36fe125e3fd79468bb729bb512926cf79
Krzysztof Jan Modras <chrmod@chrmod.net>
2021-02-20 15:42:13 +0100

    prevent updates

--- brands/ghostery/branding/pref/firefox-branding.js
+++ brands/ghostery/branding/pref/firefox-branding.js
@@ -186,3 +186,5 @@ pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", fal
 // Number of usages of the web console.
 // If this is less than 5, then pasting code into the web console is disabled
 pref("devtools.selfxss.count", 0);
+
+pref("app.update.channel", "experimental");
