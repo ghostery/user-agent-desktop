d60a1b89202f8dcc28f132e097d6c75e27d58c8a
yokoffing <hjstephens09@gmail.com>
2021-08-10 13:22:15 -0400

    Link to BetterFox descriptions
    Instead of recreating / copy+paste descriptions of prefs for Ghostery Browser, I'm going to permalink the descriptions of prefs in BetterFox.

--- brands/ghostery/branding/pref/Better-Fox.js
+++ brands/ghostery/branding/pref/Better-Fox.js
@@ -34,59 +34,34 @@ pref("dom.battery.enabled", false);
 pref("security.pki.crlite_mode", 2);
 pref("security.remote_settings.crlite_filters.enabled", true);
 
-// Microphone and camera kill switch (#370) /* experimental */
+// Microphone and camera kill switch (#370)
+// [EXPERIMENTAL]
 pref("privacy.webrtc.globalMuteToggles", true);
 
 // Site Isolation [aka Project Fission]
-// Creates operating system process-level boundaries for all sites loaded in Firefox for Desktop. Isolating each site
-// into a separate operating system process makes it harder for malicious sites to read another site’s secret or private data.
-// [1] https://hacks.mozilla.org/2021/05/introducing-firefox-new-site-isolation-security-architecture/
+// [EXPERIMENTAL]
+// https://github.com/yokoffing/Better-Fox/blob/77128d3d49ba371cb428209c053c35135ba3e4b8/SecureFox.js#L44-L47
 pref("fission.autostart", true);
 
 // State Paritioning [aka Dynamic First-Party Isolation (dFPI)]
-// Firefox manages client-side state (i.e., data stored in the browser) to mitigate the ability of websites to abuse state
-// for cross-site tracking. This effort aims to achieve that by providing what is effectively a "different", isolated storage
-// location to every website a user visits.
-// dFPI is a more web-compatible version of FPI, which double keys all third-party state by the origin of the top-level
-// context. dFPI isolates user's browsing data for each top-level eTLD+1, but is flexible enough to apply web
-// compatibility heuristics to address resulting breakage by dynamically modifying a frame's storage principal.
-// dFPI isolates most sites while applying heuristics to allow sites through the isolation in certain circumstances for usability.
-// [NOTE] dFPI partitions all of the following caches by the top-level site being visited: HTTP cache, image cache,
-// favicon cache, HSTS cache, OCSP cache, style sheet cache, font cache, DNS cache, HTTP Authentication cache,
-// Alt-Svc cache, and TLS certificate cache.
-// [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1549587
-// [2] https://developer.mozilla.org/en-US/docs/Mozilla/Firefox/Privacy/State_Partitioning
-// [3] https://blog.mozilla.org/security/2021/02/23/total-cookie-protection/
-// [4] https://hacks.mozilla.org/2021/02/introducing-state-partitioning/
+// https://github.com/yokoffing/Better-Fox/blob/77128d3d49ba371cb428209c053c35135ba3e4b8/SecureFox.js#L50-L64
 pref("network.cookie.cookieBehavior", 5);
 
 // Network Partitioning /* default pref, but keeping here for reference */
-// Networking-related APIs are not intended to be used for websites to store data, but they can be abused for
-// cross-site tracking. Network APIs and caches are permanently partitioned by the top-level site.
-// Network Partitioning (isolation) will allow Firefox to associate resources on a per-website basis rather than together
-// in the same pool. This includes cache, favicons, CSS files, images, and even speculative connections. 
-// [1] https://www.zdnet.com/article/firefox-to-ship-network-partitioning-as-a-new-anti-tracking-defense/
-// [2] https://developer.mozilla.org/en-US/docs/Web/Privacy/State_Partitioning#network_partitioning
-// [3] https://blog.mozilla.org/security/2021/01/26/supercookie-protections/
+// https://github.com/yokoffing/Better-Fox/blob/77128d3d49ba371cb428209c053c35135ba3e4b8/SecureFox.js#L69-L76
 pref("privacy.partition.network_state", true);
 
 // Redirect Tracking Prevention /* default pref, but keeping here for reference */
-// All storage is cleared (more or less) daily from origins that are known trackers and that
-// haven’t received a top-level user interaction (including scroll) within the last 45 days.
-// [1] https://www.ghacks.net/2020/08/06/how-to-enable-redirect-tracking-in-firefox/
-// [2] https://www.cookiestatus.com/firefox/#other-first-party-storage
-// [3] https://developer.mozilla.org/en-US/docs/Mozilla/Firefox/Privacy/Redirect_tracking_protection
-// [4] https://www.ghacks.net/2020/03/04/firefox-75-will-purge-site-data-if-associated-with-tracking-cookies/
-// [5] https://github.com/arkenfox/user.js/issues/1089
+// https://github.com/yokoffing/Better-Fox/blob/77128d3d49ba371cb428209c053c35135ba3e4b8/SecureFox.js#L79-L86
 pref("privacy.purge_trackers.enabled", true);
 
 // Enable Local Storage Next Generation (LSNG) (DOMStorage) 
 pref("dom.storage.next_gen", true);
 
-// Samesite Cookies /* experimental */
+// Samesite Cookies
 pref("network.cookie.sameSite.laxByDefault", true);
 pref("network.cookie.sameSite.noneRequiresSecure", true);
-// pref("network.cookie.sameSite.schemeful", true);
+pref("network.cookie.sameSite.schemeful", true);
 
 
 /** CLEARING HISTORY DEFAULTS (MANUALLY) ***/
@@ -114,78 +89,35 @@ pref("privacy.sanitize.timeSpan", 0);
 // pre-connections while preserving privacy. Users may harden these settings to their preference.
 // For more information, see "State Paritioning" and "Network Partitioning".
 
-// Network Predictor
-// Keeps track of components that were loaded during page visits so that the browser knows next time
-// which resources to request from the server: It uses a local file to remember which resources were
-// needed when the user visits a webpage (such as image.jpg and script.js), so that the next time the
-// user mouseovers a link to that webpage, this history can be used to predict what resources will
-// be needed rather than wait for the document to link those resources.
-// [NOTE] DNS pre-resolve and TCP preconnect (which includes SSL handshake). Honors settings in Private Browsing to erase data.
-// [1] https://wiki.mozilla.org/Privacy/Reviews/Necko
-// [2] https://www.ghacks.net/2014/05/11/seer-disable-firefox/
-// [3] https://github.com/dillbyrne/random-agent-spoofer/issues/238#issuecomment-110214518
-// [4] https://www.igvita.com/posa/high-performance-networking-in-google-chrome/#predictor
+// Network Predictor /* default pref, but keeping here for reference */
+// https://github.com/yokoffing/Better-Fox/blob/079be70df3e513507dc419c3cce1a413902ada13/SecureFox.js#L184-L195
 pref("network.predictor.enabled", true); // default
-// Fetch critical resources on the page ahead of time as determined by the local file, to accelerate rendering of the page.
 pref("network.predictor.enable-hover-on-ssl", true);
 pref("network.predictor.enable-prefetch", true);
 
 // DNS pre-resolve <link rel="dns-prefetch">
-// Resolve hostnames ahead of time, to avoid DNS latency.
-// [1] https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-// [2] https://css-tricks.com/prefetching-preloading-prebrowsing/#dns-prefetching
-// [3] http://www.mecs-press.org/ijieeb/ijieeb-v7-n5/IJIEEB-V7-N5-2.pdf
+// https://github.com/yokoffing/Better-Fox/blob/e9535084374c4f379bc20fda945b3236b7723c48/SecureFox.js#L201-L206
 pref("network.dns.disablePrefetch", true);
 pref("network.dns.disablePrefetchFromHTTPS", false);
 
-// Preconnect to the autocomplete URL in the address bar
-// Firefox preloads URLs that autocomplete when a user types into the address bar.
-// Connects to destination server ahead of time, to avoid TCP handshake latency.
-// [NOTE] Firefox will perform DNS lookup and TCP and TLS handshake, but will not start sending or receiving HTTP data.
-// [1] https://www.ghacks.net/2017/07/24/disable-preloading-firefox-autocomplete-urls/
+// Preconnect to the autocomplete URL in the address bar /* default pref, but keeping here for reference */
+// https://github.com/yokoffing/Better-Fox/blob/079be70df3e513507dc419c3cce1a413902ada13/SecureFox.js#L209-L213
 pref("browser.urlbar.speculativeConnect.enabled", true); // default
 
-// Link prefetching <link rel="prefetch">
-// Fetch critical resources on the page ahead of time, to accelerate rendering of the page.
-// Websites can provide Firefox with hints as to which page is likely the be accessed next so that it is downloaded right away,
-// even if you don't request that link. The prefetch resource hint tells the browser to go grab a resource even though it
-// hasn’t been requested by the current page, and puts it into cache. Firefox will request the resource at a low
-// priority and only during idle time so that the resource doesn’t compete with anything needed for the current navigation.
-// When the user clicks on a link, or initiates any kind of page load, link prefetching will stop and any prefetch hints will be discarded.
-// [1] https://developer.mozilla.org/en-US/docs/Web/HTTP/Link_prefetching_FAQ#Privacy_implications
-// [2] http://www.mecs-press.org/ijieeb/ijieeb-v7-n5/IJIEEB-V7-N5-2.pdf
-// [3] https://timkadlec.com/remembers/2020-06-17-prefetching-at-this-age/
+// Link prefetching <link rel="prefetch"> /* default pref, but keeping here for reference */
+// https://github.com/yokoffing/Better-Fox/blob/079be70df3e513507dc419c3cce1a413902ada13/SecureFox.js#L216-L225
 pref("network.prefetch-next", true); // default
 
-// Prefetch links upon hover
-// When you hover over links, connections are established to linked domains and servers automatically to speed up the loading
-// process should you click on the link. To improve the loading speed, Firefox will open predictive connections to sites when
-// the user hovers their mouse over. In case the user follows through with the action, the page can begin loading faster since
-// some of the work was already started in advance.
-// [NOTE] TCP and SSL handshakes are set up in advance but page contents are not downloaded until a click on the link is registered.
-// [1] https://news.slashdot.org/story/15/08/14/2321202/how-to-quash-firefoxs-silent-requests
-// [2] https://www.ghacks.net/2015/08/16/block-firefox-from-connecting-to-sites-when-you-hover-over-links
+// Prefetch links upon hover /* default pref, but keeping here for reference */
+// https://github.com/yokoffing/Better-Fox/blob/079be70df3e513507dc419c3cce1a413902ada13/SecureFox.js#L228-L235
 pref("network.http.speculative-parallel-limit", 6); // default
 
 // Preload <link rel=preload>
-// Fetch the entire page with all of its resources ahead of time, to enable instant navigation when triggered by the user.
-// Allows developers to hint to the browser to preload some resources with a higher priority and in advance, which helps the web page to
-// render and get into the stable and interactive state faster. This spec assumes that sometimes it’s best to always download an asset,
-// regardless of whether the browser thinks that’s a good idea or not(!). Unlike prefetching assets, which can be ignored, preloading assets
-// must be requested by the browser.
-// [WARNING] Interferes with content blocking extensions, even if you utilize DNS-level blocking as well. Disable this!
-// [NOTE] Revisit and test to see if we can enable this in future builds.
-// [1] https://www.janbambas.cz/firefox-enables-link-rel-preload-support/
-// [2] https://bugzilla.mozilla.org/show_bug.cgi?id=1639607
-// [3] https://css-tricks.com/prefetching-preloading-prebrowsing/#future-option-preloading
+// https://github.com/yokoffing/Better-Fox/blob/079be70df3e513507dc419c3cce1a413902ada13/SecureFox.js#L238-L247
 pref("network.preload", false);
 
 // New tab preload
-// [WARNING] Disabling this causes a delay when opening a new tab in Firefox.
-// [NOTE] Not sure if this affects Ghostery since we use an extension for the New Tab page. Needs testing.
-// [1] https://wiki.mozilla.org/Tiles/Technical_Documentation#Ping
-// [2] https://gecko.readthedocs.org/en/latest/browser/browser/DirectoryLinksProvider.html#browser-newtabpage-directory-source
-// [3] https://gecko.readthedocs.org/en/latest/browser/browser/DirectoryLinksProvider.html#browser-newtabpage-directory-ping
+// https://github.com/yokoffing/Better-Fox/blob/079be70df3e513507dc419c3cce1a413902ada13/SecureFox.js#L250-L254
 pref("browser.newtab.preload", true); // default
 
 
@@ -200,12 +132,7 @@ pref("browser.search.suggest.enabled", false);
 pref("browser.search.suggest.enabled.private", false);
 
 // URL bar domain guessing
-// Domain guessing intercepts DNS "hostname not found errors" and resends a
-// request (e.g. by adding www or .com). This is inconsistent use (e.g. FQDNs), does not work
-// via Proxy Servers (different error), is a flawed use of DNS (TLDs: why treat .com
-// as the 411 for DNS errors?), privacy issues (why connect to sites you didn't
-// intend to), can leak sensitive data (e.g. query strings: e.g. Princeton attack),
-// and is a security risk (e.g. common typos & malicious sites set up to exploit this).
+// https://github.com/yokoffing/Better-Fox/blob/079be70df3e513507dc419c3cce1a413902ada13/SecureFox.js#L276-L281
 pref("browser.fixup.alternate.enabled", false);
 
 // "Not Secure" text in the URL bar on HTTP sites.
