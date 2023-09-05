5589d31de7b51ac5563e6a2126a2f88beace3846
Krzysztof Modras <chrmod@chrmod.net>
2023-09-05 19:26:10 +0200

    Update MacOS SDK

--- ci/build-helpers.groovy
+++ ci/build-helpers.groovy
@@ -9,7 +9,7 @@ def build(opts, Closure postpackage={}, Closure archiving={}) {
 
         stage('prepare') {
             download('makecab.exe')
-            download('MacOSX13.0.sdk.tar.bz2')
+            download('MacOSX13.3.sdk.tar.bz2')
         }
 
         def image = stage('docker build base') {
@@ -37,7 +37,7 @@ def build(opts, Closure postpackage={}, Closure archiving={}) {
                 }
 
                 dir('mozilla-release') {
-                    sh 'rm -f `pwd`/MacOSX13.0.sdk; ln -s /builds/worker/fetches/MacOSX13.0.sdk `pwd`/MacOSX13.0.sdk'
+                    sh 'rm -f `pwd`/MacOSX13.3.sdk; ln -s /builds/worker/fetches/MacOSX13.3.sdk `pwd`/MacOSX13.3.sdk'
 
                     if (opts.PGO) {
                         stage("${opts.name}: fetch profiles") {
